bc0f5b7db3553840035673121c7e1ccb7ccaab2a
Ashwath2348 <ashwathhombali31@gmail.com>
2025-03-10 16:16:12 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,35 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
-const app = express();
+function openLoginPage() {
+    document.getElementById("loginModal").style.display = "flex";
+}
 
-// Middleware to parse JSON requests
-app.use(bodyParser.json());
-
-// Endpoint to compile C code
-app.post('/compile', (req, res) => {
-    const { code } = req.body;
-
-    if (!code) {
-        return res.status(400).send({ error: 'No code provided' });
-    }
-
-    // Save code to a temporary file
-    const fs = require('fs');
-    const filePath = './temp_code.c';
-
-    fs.writeFileSync(filePath, code, 'utf8');
-
-    // Compile the C code using gcc
-    exec(`gcc ${filePath} -o output && ./output`, (error, stdout, stderr) => {
-        if (error) {
-            return res.status(500).send({ error: stderr || error.message });
-        }
-        res.send({ result: stdout });
-    });
-});
-
-// Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+function closeLoginPage() {
+    document.getElementById("loginModal").style.display = "none";
+}
