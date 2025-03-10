00c84724500b1898af24ca69ebddf7850b469162
Ashwath2348 <ashwathhombali31@gmail.com>
2025-03-10 10:52:00 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,14 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
+// Animate Header on Scroll
+window.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.style.transform = "translateY(-100px)";
+    } else {
+        header.style.transform = "translateY(0)";
+    }
+});
+
+// Menu Toggle for Mobile
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+    document.querySelector(".nav-links").classList.toggle("active");
+});
